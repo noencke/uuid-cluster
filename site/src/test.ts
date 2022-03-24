5360c618f5b1c357b2a6362c282cb617d095ee0d
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2022-03-24 10:44:01 -0700

    simplify repro

--- site/src/test.ts
+++ site/src/test.ts
@@ -1,15 +1,9 @@
 async function doLoop(): Promise<void> {
   const uuid_cluster = await import("uuid-cluster");
-  const compressor = new uuid_cluster.ClusterCompressor();
-  compressor.add(0, 0, 0);
-  for (let i = 1; i <= 5000; i++) {
-    const thingy = compressor.compress(6);
-
-    if (i % 1000 === 0) {
-      console.warn(i);
-    }
+  for (let i = 1; i <= 1000; i++) {
+    uuid_cluster.leak_test();
   }
-  compressor.free();
+  console.warn("done");
 }
 
 (window as any)["doLoop"] = doLoop;
