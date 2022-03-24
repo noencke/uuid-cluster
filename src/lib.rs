06f87edda46885014f488d78ce79f912f6f52c3e
Craig Macomber (Microsoft) <42876482+CraigMacomber@users.noreply.github.com>
2022-03-24 13:20:43 -0700

    minify more

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,12 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn leak_test() {
     // This leaks
     CompressorOxide {
-        params: ParamsOxide::new(0),
-        dict: DictOxide::new(0),
+        params: ParamsOxide {
+            local_buf: Box::new(LocalBuf {
+                b: [0; OUT_BUF_SIZE],
+            }),
+        },
+        dict: { DictOxide { b: Box::default() } },
     };
 
     // This does not leak:
@@ -32,86 +36,13 @@ const LZ_DICT_SIZE: usize = 32_768;
 const MAX_MATCH_LEN: usize = 258;
 
 struct DictOxide {
-    /// The maximum number of checks in the hash chain, for the initial,
-    /// and the lazy match respectively.
-    pub max_probes: [u32; 2],
     /// Buffer of input data.
     /// Padded with 1 byte to simplify matching code in `compress_fast`.
     pub b: Box<HashBuffers>,
-
-    pub code_buf_dict_pos: usize,
-    pub lookahead_size: usize,
-    pub lookahead_pos: usize,
-    pub size: usize,
-}
-
-const fn probes_from_flags(flags: u32) -> [u32; 2] {
-    [
-        1 + ((flags & 0xFFF) + 2) / 3,
-        1 + (((flags & 0xFFF) >> 2) + 2) / 3,
-    ]
-}
-
-impl DictOxide {
-    pub fn new(flags: u32) -> Self {
-        DictOxide {
-            max_probes: probes_from_flags(flags),
-            b: Box::default(),
-            code_buf_dict_pos: 0,
-            lookahead_size: 0,
-            lookahead_pos: 0,
-            size: 0,
-        }
-    }
 }
 
 struct ParamsOxide {
-    pub flags: u32,
-    pub greedy_parsing: bool,
-    pub block_index: u32,
-
-    pub saved_match_dist: u32,
-    pub saved_match_len: u32,
-    pub saved_lit: u8,
-
-    pub flush_ofs: u32,
-    pub flush_remaining: u32,
-    pub finished: bool,
-
-    pub adler32: u32,
-
-    pub src_pos: usize,
-
-    pub out_buf_ofs: usize,
-    pub prev_return_status: i32,
-
-    pub saved_bit_buffer: u32,
-    pub saved_bits_in: u32,
-
     pub local_buf: Box<LocalBuf>,
-}
-
-impl ParamsOxide {
-    pub fn new(flags: u32) -> Self {
-        ParamsOxide {
-            flags,
-            greedy_parsing: false,
-            block_index: 0,
-            saved_match_dist: 0,
-            saved_match_len: 0,
-            saved_lit: 0,
-            flush_ofs: 0,
-            flush_remaining: 0,
-            finished: false,
-            adler32: 0,
-            src_pos: 0,
-            out_buf_ofs: 0,
-            prev_return_status: 0,
-            saved_bit_buffer: 0,
-            saved_bits_in: 0,
-            local_buf: Box::default(),
-        }
-    }
 }
 
 /// Size of the buffer of lz77 encoded data.
@@ -138,12 +69,4 @@ impl Default for HashBuffers {
 
 struct LocalBuf {
     pub b: [u8; OUT_BUF_SIZE],
-}
-
-impl Default for LocalBuf {
-    fn default() -> LocalBuf {
-        LocalBuf {
-            b: [0; OUT_BUF_SIZE],
-        }
-    }
 }
